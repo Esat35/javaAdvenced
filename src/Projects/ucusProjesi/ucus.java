049996446cf61ccde1ff43cfd37f61496840844a
Esat35 <dugindugin35@gmail.com>
2022-03-29 18:03:14 +0300

    29.03.22

---
+++ src/Projects/ucusProjesi/ucus.java
@@ -0,0 +1,30 @@
+package Projects.ucusProjesi;
+
+public class ucus {
+    /*
+    A şehrinden uçmak isteyen bir yolcu B şehrine 500km C şehrine  700km  D şehrine  900 km mesafededir.
+    Bilet tarifesi:
+    km birim fiyati : 0.10$
+    yolcu 12 yasindan kucukse toplam fiyat %50 indirim,
+    12 ve 24 yas arasindaysa 10% indirim,
+    65 yasindan buyukse 30% indirim,
+    bilet gidis donus alinirrsa 20% indirim uygulayan bi app create ediniz
+
+ */
+
+    public static void main(String[] args) {
+
+        int AB=500;
+        int AC=700;
+        int AD=900;
+
+
+
+
+
+    }
+
+
+
+
+}
---
+++ src/tasks/T01.java
@@ -0,0 +1,41 @@
+package tasks;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class T01 {
+    public static void main(String[] args) {
+        /*
+         * Kullanicidan gelen cumleyi  parametre olarak alan
+         * ve cumledeki en uzun kelimeyi return eden LongestWord(sentence) methodu create ediniz.
+         * Trick: Aynı uzunlukta iki veya daha fazla kelime varsa, String'deki en uzun ilk sözcüğü return edilsin
+         * Noktalama işaretlerini dikkate almayın ve cumlenin boş olmayacağını varsayın.
+         */
+Scanner scan=new Scanner(System.in);
+        System.out.print("bir cumle gırınız : ");
+        String cumle= scan.nextLine();
+
+       String enUzunKlime= LongestWord(cumle);
+        System.out.println("cumledekı en uzun kelıme : "+enUzunKlime);
+
+    }
+
+    private static String LongestWord(String cumle) {
+        String arr[]=cumle.split(" ");
+        System.out.println(Arrays.toString(arr));
+        int kelimeUzunlugu=0;
+        String uzunKelime="";
+
+        for (int i=0; i<arr.length; i++) {
+           arr[i]= arr[i].replaceAll("\\W","");
+           if (arr[i].length()>kelimeUzunlugu){
+               uzunKelime=arr[i];
+               kelimeUzunlugu=arr[i].length();
+           }
+
+            }
+        System.out.println(Arrays.toString(arr));
+
+        return uzunKelime;
+    }
+}
